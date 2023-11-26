5d5b22f37a40ab7c2af2d8a47ba700e33441f84c
x7k3e21 <uly4nkin.denis@gmail.com>
2023-11-26 20:30:23 +0300

    Several changes to shoot command

--- sources/telegram/commands/roleplay/shoot.js
+++ sources/telegram/commands/roleplay/shoot.js
@@ -4,7 +4,7 @@ module.exports.aliases = [ "shoot" ];
 module.exports.execute = async (context) =>
 {
     const requestUser = context.from.username;
-    const responseUser = context.message.split(" ")[1];
+    const responseUser = context.message.text.split(" ")[1];
 
-    await context.reply(`${responseUser} was shot by ${requestUser}`);
+    await context.reply(`${responseUser} was shot by @${requestUser}`);
 }
