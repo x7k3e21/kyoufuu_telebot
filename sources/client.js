2cdfaccf60b169bd2386b28481fcbb7f94baa17b
x7k3e21 <uly4nkin.denis@gmail.com>
2024-03-01 01:53:06 +0300

    Added /command handler

--- sources/client.js
+++ sources/client.js
@@ -2,8 +2,6 @@
 const grammy = require("grammy");
 const express = require("express");
 
-const chat_members = require("@grammyjs/chat-members");
-
 const os = require("node:os");
 const fs = require("node:fs");
 
@@ -13,88 +11,71 @@ const processID = process.pid;
 
 const client = new grammy.Bot(process.env.CLIENT_TOKEN || "");
 
-const chatMembersAdapter = new grammy.MemorySessionStorage();
-client.use(chat_members.chatMembers(chatMembersAdapter));
+const clientConfig = require("../config.json");
 
-const config = require("../config.json");
+const commandsPath = path.join(__dirname, clientConfig.client.paths.commandsPath);
+const queriesPath = path.join(__dirname, clientConfig.client.paths.queriesPath);
 
-const commandsPath = path.join(__dirname, config.client.commands);
-const queriesPath = path.join(__dirname, config.client.queries);
-
-const middlewarePath = path.join(__dirname, config.server.middleware);
+const clientPrefix = clientConfig.client.commandsPrefix;
 
 const commandsList = fs.readdirSync(commandsPath, { recursive: true });
+const queriesList = fs.readdirSync(queriesPath, { recursive: true });
 
-for (let commandFile of commandsList)
+const commandsMap = new Map();
+
+for(let commandFile of commandsList)
 {
-    if(!commandFile.endsWith(".js"))
-    {
-        continue;
-    }
+    if(!commandFile.endsWith(".js")) continue;
 
     commandFile = commandFile.replace("\\", "/");
-    console.log(`Found command ${commandFile}`);
 
-    const commandFilePath = path.join(commandsPath, commandFile);
+    const commandPath = path.join(commandsPath, commandFile);
 
-    try 
+    try
     {
-        const commandModule = require(commandFilePath);
-        client.command(commandModule.aliases, commandModule.execute);
-    
-        console.log(`Configured command /${commandModule.aliases[0]}`);
-    } 
-    catch (error) 
+        const commandModule = require(commandPath);
+
+        commandsMap.set(commandModule.name, commandModule.execute);
+
+        for(let alias of commandModule.aliases)
+        {
+            commandsMap.set(alias, commandModule.execute);
+        }
+
+        console.log(`Successfully configured ${commandFile}`);
+    }
+    catch(error)
     {
         console.log(`Failed to configure command ${commandFile}`);
     }
 }
 
+client.on("message:text", async (context) =>
+{
+    const messageContent = context.message.text.split(" ");
+
+    const commandName = messageContent[0];
+    const commandArgs = messageContent.slice(1);
+
+    if(commandName.startsWith(clientPrefix))
+    {
+        const commandScript = commandsMap.get(commandName.slice(1));
+
+        if(commandScript != undefined)
+        {
+            commandScript(context);
+        }
+    }
+});
+
 if(process.env.NODE_ENV == "production")
 {
-    const dynamodb = require("@cyclic.sh/dynamodb");
-    const database = dynamodb(process.env.CYCLIC_DB);
-
-    let usersCollection = database.collection("users");
-
-    client.use(async (context, handler) =>
-    {
-        context.database = { users: usersCollection };
-
-        await handler();
-    });
-
     const application = express();
 
     application.use(express.json());
     application.use("/client/callback", grammy.webhookCallback(client, "express"));
-    
-    const middlewareList = fs.readdirSync(middlewarePath, { recursive: true });
 
-    for(let middlewareFile of middlewareList)
-    {
-        if(!middlewareFile.endsWith(".js"))
-        {
-            continue;
-        }
-
-        middlewareFile = middlewareFile.replace("\\", "/");
-        console.log(`Linking middleware ${middlewareFile}`);
-
-        const middlewareFilePath = path.join(middlewarePath, middlewareFile);
-    
-        try 
-        {
-            const middlewareModule = require(middlewareFilePath);
-            application.use(middlewareModule.middleware);
-        
-            console.log(`Linked middleware ${middlewareFile}`);
-        } 
-        catch (error) 
-        {
-            console.log(`Failed to link middleware ${middlewareFile}`);
-        }
-    }
+    application.set("view engine", "hbs");
 
     application.get("/debug", (request, response) =>
     {
